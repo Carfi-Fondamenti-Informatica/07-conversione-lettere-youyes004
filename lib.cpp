fa2378827819a712c4e98c732e83dc528e935272
Yousef El Sayed <115503073+youyes004@users.noreply.github.com>
2022-12-08 17:11:53 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -2,12 +2,12 @@
 // Created by Yousef El Sayed on 08/12/2022.
 //
 #include "lib.h"
-bool convert(char & a){
+bool convert (char & a){
     if(a>=65 && a<=90) {
-        a+=32;
+        a +=32;
         return true;
     }else if(a>=97 && a<=122){
-       a-=32;
+       a -=32;
        return true;
     }
     return false;
